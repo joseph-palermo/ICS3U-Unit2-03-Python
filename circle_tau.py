452e02a0d0245cc6d916bf8321f91bbbcc4f1eda
Ubuntu <ubuntu@ip-172-31-31-42.ec2.internal>
2019-09-24 01:38:51 +0000

    Initial commit

---
+++ circle_tau.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+# Created by: Joseph Palermo
+# Created on: September 2019
+# My circle with tau
+
+
+import constants
+
+
+print("Enter radius of circle (mm): 100")
+
+
+def main():
+    print("Circumference is {}mm" .format(100*constants.TAU))  # The circumference
+
+
+if __name__ == "__main__":
+    main()
